9c098f68d8c2dadc6403d92e13af01e9bf0bbdf0
hafizmuttiullah786 <muttiullah1@gmail.com>
2026-01-29 13:04:56 +0500

    video call design fixes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   return (
     <main>
       <Navbar />
-      <div className="flex min-h-screen gap-0 md:gap-5">
+      <div className="flex min-h-screen gap-0">
         <ToastContainer />
         <DashboardNavbar />
         <div className="w-full overflow-y-auto">{children}</div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,33 +10,33 @@ export default function Navbar() {
   const { data } = useGetMeQuery("");
   return (
     <nav
-      className={` mx-auto border-b border-[#b9b9b9] h-24 flex px-5 md:px-28 fixed bg-white z-[9] w-full`}
+      className={`mx-auto border-b border-[#b9b9b9] py-3 flex px-5 md:px-[40px] fixed bg-white z-[9] w-full`}
     >
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between w-full items-center">
         <div className="hidden md:block relative  flex-1">
           <Link href={"/"}>
             <Image
               src={logo}
               alt="Logo"
-              className="object-contain h-full"
+              className="object-contain h-[50px] w-[50px]"
               priority
             />
           </Link>
         </div>
         <div></div>
 
-        <div className=" flex items-center gap-3">
+        <div className="flex items-center gap-3">
           <Image
             src={profileImage}
             alt="Logo"
-            className="h-14 w-14 rounded-full border-2 border-primaryColor"
+            className="h-[45px] w-[45px] rounded-full border-2 border-primaryColor"
             priority
           />
           <div>
             <h1 className="text-[#171717] text-base font-semibold leading-normal">
               {data?.data?.name || "Admin"}
             </h1>
-            <h1 className="text-[#747474] text-base font-normal leading-normal">
+            <h1 className="text-[#747474] text-base font-normal leading-normal text-sm">
               {data?.data?.role || "Admin"}
             </h1>
           </div>
